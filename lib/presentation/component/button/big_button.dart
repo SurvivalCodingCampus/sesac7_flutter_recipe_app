12ce95c4288803a50d7da3148608cd5ee61d5dcb
Moon <1tjsen@naver.com>
2025-07-15 14:43:59 +0900

    feat: BigButton 추가

---
+++ lib/presentation/component/button/big_button.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_recipe_app/ui/app_colors.dart';
+import 'package:flutter_recipe_app/ui/text_styles.dart';
+
+class BigButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onClick;
+
+  const BigButton({
+    super.key,
+    required this.text,
+    required this.onClick,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onClick,
+      child: Container(
+        width: 315,
+        height: 60,
+        decoration: BoxDecoration(
+          color: AppColors.primary100,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          spacing: 11,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 114,
+              child: Center(
+                child: Text(
+                  'Button',
+                  style: TextStyles.normalTextBold.copyWith(
+                    color: AppColors.white,
+                  ),
+                ),
+              ),
+            ),
+            Icon(
+              Icons.arrow_forward,
+              color: AppColors.white,
+              size: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
