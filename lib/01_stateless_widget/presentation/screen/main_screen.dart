c52127fe87282447e2c81f044599e91c978d2a26
Hagmin.Lee <iilhm2001@naver.com>
2025-07-15 22:04:31 +0900

    feat: Label 세 가지 구성의 탭 추가 및 Reformat Code

--- lib/01_stateless_widget/presentation/screen/main_screen.dart
+++ lib/01_stateless_widget/presentation/screen/main_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_recipe_app/01_stateless_widget/presentation/component/left_selected_triple_tabs.dart';
+import 'package:flutter_recipe_app/01_stateless_widget/presentation/component/middle_selected_triple_tabs.dart';
+import 'package:flutter_recipe_app/01_stateless_widget/presentation/component/right_selected_triple_tabs.dart';
 import 'package:flutter_recipe_app/01_stateless_widget/presentation/component/small_button.dart';
 import 'package:flutter_recipe_app/01_stateless_widget/presentation/component/big_button.dart';
 import 'package:flutter_recipe_app/01_stateless_widget/presentation/component/default_input_field.dart';
@@ -19,21 +22,94 @@ class MainScreen extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              BigButton(text: 'Button', onClick: () { print('큰 버튼 클릭되었어요'); }),
-              SizedBox(height: 20),
-              MediumButton(text: 'Button', onClick: () { print('중간 버튼 클릭되었어요'); }),
-              SizedBox(height: 20),
-              SmallButton(text: 'Button', onClick: () { print('작은 버튼 클릭되었어요'); },),
-              SizedBox(height: 20),
-              DefaultInputField(label: 'Label', placeHolder: 'PlaceHolder', value: 'PlaceHolder', onValueChange: (String value) { print(value); }),
-              SizedBox(height: 20),
-              FocusedInputField(label: 'Label', placeHolder: 'PlaceHolder', value: 'PlaceHolder', onValueChange: (String value) { print(value); }),
-              SizedBox(height: 20),
-              FilledInputField(label: 'Label', placeHolder: 'PlaceHolder', value: 'PlaceHolder', onValueChange: (String value) { print(value); }),
-              SizedBox(height: 20),
-              LeftSelectedTabs(listOfLables: ['label1', 'label2'], selectedIndex: 0, onValueChange: (int value) { print('selected: $value'); },),
-              SizedBox(height: 20),
-              RightSelectedTabs(listOfLables: ['label1', 'label2'], selectedIndex: 0, onValueChange: (int value) { print('selected: $value'); },),
+              SizedBox(height: 60),
+              BigButton(
+                text: 'Button',
+                onClick: () {
+                  print('큰 버튼 클릭되었어요');
+                },
+              ),
+              SizedBox(height: 13),
+              MediumButton(
+                text: 'Button',
+                onClick: () {
+                  print('중간 버튼 클릭되었어요');
+                },
+              ),
+              SizedBox(height: 13),
+              SmallButton(
+                text: 'Button',
+                onClick: () {
+                  print('작은 버튼 클릭되었어요');
+                },
+              ),
+              SizedBox(height: 13),
+              DefaultInputField(
+                label: 'Label',
+                placeHolder: 'PlaceHolder',
+                value: 'PlaceHolder',
+                onValueChange: (String value) {
+                  print(value);
+                },
+              ),
+              SizedBox(height: 13),
+              FocusedInputField(
+                label: 'Label',
+                placeHolder: 'PlaceHolder',
+                value: 'PlaceHolder',
+                onValueChange: (String value) {
+                  print(value);
+                },
+              ),
+              SizedBox(height: 13),
+              FilledInputField(
+                label: 'Label',
+                placeHolder: 'PlaceHolder',
+                value: 'PlaceHolder',
+                onValueChange: (String value) {
+                  print(value);
+                },
+              ),
+              SizedBox(height: 13),
+              LeftSelectedTabs(
+                listOfLables: ['label1', 'label2'],
+                selectedIndex: 0,
+                onValueChange: (int value) {
+                  print('selected: $value');
+                },
+              ),
+              SizedBox(height: 13),
+              RightSelectedTabs(
+                listOfLables: ['label1', 'label2'],
+                selectedIndex: 0,
+                onValueChange: (int value) {
+                  print('selected: $value');
+                },
+              ),
+              SizedBox(height: 13),
+              LeftSelectedTripleTabs(
+                listOfLables: ['label1', 'label2', 'label3'],
+                selectedIndex: 0,
+                onValueChange: (int value) {
+                  print('selected: $value');
+                },
+              ),
+              SizedBox(height: 13),
+              MiddleSelectedTripleTabs(
+                listOfLables: ['label1', 'label2', 'label3'],
+                selectedIndex: 0,
+                onValueChange: (int value) {
+                  print('selected: $value');
+                },
+              ),
+              SizedBox(height: 13),
+              RightSelectedTripleTabs(
+                listOfLables: ['label1', 'label2', 'label3'],
+                selectedIndex: 0,
+                onValueChange: (int value) {
+                  print('selected: $value');
+                },
+              ),
             ],
           ),
         ),
