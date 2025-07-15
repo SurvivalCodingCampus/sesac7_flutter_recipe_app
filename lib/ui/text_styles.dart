68e43edc9a2298c9423fca7fce32bbacc36ad405
Jaesung lim <7newer@gmail.com>
2025-07-15 14:48:11 +0900

    fix: text_styles.dart height 명시된 px로 변경

--- lib/ui/text_styles.dart
+++ lib/ui/text_styles.dart
@@ -5,49 +5,49 @@ abstract class TextStyles {
     fontFamily: 'Poppins',
     fontWeight: FontWeight.bold,
     fontSize: 50,
-    height: 1.5, // 75 / 50 -> 줄 높이 = 50 * 1.5 = 75px
+    height: 75, // 75 / 50 -> 줄 높이 = 50 * 1.5 = 75px
   );
 
   static const TextStyle headerTextBold = TextStyle(
     fontFamily: 'Poppins',
     fontWeight: FontWeight.bold,
     fontSize: 30,
-    height: 1.5, // 45 / 30
+    height: 45,
   );
 
   static const TextStyle largeTextBold = TextStyle(
     fontFamily: 'Poppins',
     fontWeight: FontWeight.bold,
     fontSize: 20,
-    height: 1.5, // 30 / 20
+    height: 30,
   );
 
   static const TextStyle mediumTextBold = TextStyle(
     fontFamily: 'Poppins',
     fontWeight: FontWeight.bold,
     fontSize: 18,
-    height: 1.5, // 27 / 18
+    height: 27,
   );
 
   static const TextStyle normalTextBold = TextStyle(
     fontFamily: 'Poppins',
     fontWeight: FontWeight.bold,
     fontSize: 16,
-    height: 1.5, // 24 / 16
+    height: 24,
   );
 
   static const TextStyle smallTextBold = TextStyle(
     fontFamily: 'Poppins',
     fontWeight: FontWeight.bold,
     fontSize: 14,
-    height: 1.5, // 21 / 14
+    height: 21,
   );
 
   static const TextStyle smallerTextBold = TextStyle(
     fontFamily: 'Poppins',
     fontWeight: FontWeight.bold,
     fontSize: 11,
-    height: 1.545, // 17 / 11
+    height: 17,
   );
 
   // Regular 스타일
@@ -55,48 +55,48 @@ abstract class TextStyles {
     fontFamily: 'Poppins',
     fontWeight: FontWeight.normal,
     fontSize: 50,
-    height: 1.5, // 75 / 50
+    height: 75,
   );
 
   static const TextStyle headerTextRegular = TextStyle(
     fontFamily: 'Poppins',
     fontWeight: FontWeight.normal,
     fontSize: 30,
-    height: 1.5, // 45 / 30
+    height: 45,
   );
 
   static const TextStyle largeTextRegular = TextStyle(
     fontFamily: 'Poppins',
     fontWeight: FontWeight.normal,
     fontSize: 20,
-    height: 1.5, // 30 / 20
+    height: 30,
   );
 
   static const TextStyle mediumTextRegular = TextStyle(
     fontFamily: 'Poppins',
     fontWeight: FontWeight.normal,
     fontSize: 18,
-    height: 1.5, // 27 / 18
+    height: 27,
   );
 
   static const TextStyle normalTextRegular = TextStyle(
     fontFamily: 'Poppins',
     fontWeight: FontWeight.normal,
     fontSize: 16,
-    height: 1.5, // 24 / 16
+    height: 24,
   );
 
   static const TextStyle smallTextRegular = TextStyle(
     fontFamily: 'Poppins',
     fontWeight: FontWeight.normal,
     fontSize: 14,
-    height: 1.5, // 21 / 14
+    height: 21,
   );
 
   static const TextStyle smallerTextRegular = TextStyle(
     fontFamily: 'Poppins',
     fontWeight: FontWeight.normal,
     fontSize: 11,
-    height: 2.45, // 27 / 11
+    height: 27,
   );
 }
