b38b7e42b9bce4f85dc0d3f548f487c509e19fb5
ParkGiTeak <rl980901@naver.com>
2025-07-15 17:50:12 +0900

    feat: 모든 TextStyle에 overflow ellipsis 추가

--- lib/ui/text_styles.dart
+++ lib/ui/text_styles.dart
@@ -1,14 +1,15 @@
-import 'dart:ui';
-
+import 'package:flutter/material.dart';
 import 'package:flutter_recipe_app/core/enum/font_family_type.dart';
 import 'package:flutter_recipe_app/ui/app_colors.dart';
 
 abstract class TextStyles {
+  /* Button Component START */
   static final TextStyle smallButtonTitle = TextStyle(
     color: AppColors.white,
     fontSize: 11,
     fontFamily: FontFamilyType.poppins.fontFamilyName,
     fontWeight: FontWeight.w600,
+    overflow: TextOverflow.ellipsis,
   );
 
   static final TextStyle mediumButtonTitle = TextStyle(
@@ -16,6 +17,7 @@ abstract class TextStyles {
     fontSize: 16,
     fontFamily: FontFamilyType.poppins.fontFamilyName,
     fontWeight: FontWeight.w600,
+    overflow: TextOverflow.ellipsis,
   );
 
   static final TextStyle bigButtonTitle = TextStyle(
@@ -23,5 +25,25 @@ abstract class TextStyles {
     fontSize: 16,
     fontFamily: FontFamilyType.poppins.fontFamilyName,
     fontWeight: FontWeight.w600,
+    overflow: TextOverflow.ellipsis,
+  );
+
+  /* Button Component END */
+
+  /* InputField START */
+  static final TextStyle inputFiledLabel = TextStyle(
+    color: AppColors.labelColor,
+    fontSize: 14,
+    fontFamily: FontFamilyType.poppins.fontFamilyName,
+    fontWeight: FontWeight.w400,
+    overflow: TextOverflow.ellipsis,
+  );
+
+  static final TextStyle inputFiledHint = TextStyle(
+    color: AppColors.gray4,
+    fontSize: 11,
+    fontFamily: FontFamilyType.poppins.fontFamilyName,
+    fontWeight: FontWeight.w400,
+    overflow: TextOverflow.ellipsis,
   );
 }
