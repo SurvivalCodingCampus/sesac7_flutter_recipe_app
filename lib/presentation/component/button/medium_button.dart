0ef068c5ecf23d3d475d17218cece0760e1878d1
ParkGiTeak <rl980901@naver.com>
2025-07-15 16:36:30 +0900

    feat: BigButton, MediumButton, SmallButton Component 추가

---
+++ lib/presentation/component/button/medium_button.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_recipe_app/ui/app_colors.dart';
+
+import '../../../ui/text_styles.dart';
+
+class MediumButton extends StatelessWidget {
+  final String title;
+  final Function(void)? onClick;
+
+  const MediumButton({
+    super.key,
+    required this.title,
+    this.onClick,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 243.0,
+      height: 54.0,
+      decoration: BoxDecoration(
+        color: AppColors.primary100,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: GestureDetector(
+        onTap: () {
+          onClick;
+        },
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 114.0,
+                height: 24.0,
+                child: Center(
+                  child: Text(title, style: TextStyles.mediumButtonTitle),
+                ),
+              ),
+              SizedBox(
+                width: 9.0,
+              ),
+              SizedBox(
+                width: 20.0,
+                height: 20.0,
+                child: Center(child: Icon(Icons.arrow_forward)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/component/button/small_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_recipe_app/ui/app_colors.dart';
+import 'package:flutter_recipe_app/ui/text_styles.dart';
+
+class SmallButton extends StatelessWidget {
+  final String title;
+  final Function(void)? onClick;
+
+  const SmallButton({
+    super.key,
+    required this.title,
+    this.onClick,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 174.0,
+      height: 37.0,
+      decoration: BoxDecoration(
+        color: AppColors.primary100,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: GestureDetector(
+        onTap: () {
+          onClick;
+        },
+        child: Center(
+          child: Text(title, style: TextStyles.smallButtonTitle),
+        ),
+      ),
+    );
+  }
+}
