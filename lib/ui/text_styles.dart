e39f76c8ca13866b80a694ea69cda2a53bbd61be
Moon <1tjsen@naver.com>
2025-07-15 21:47:39 +0900

    refactor: TextStyles 구글 폰트로 수정

--- lib/ui/text_styles.dart
+++ lib/ui/text_styles.dart
@@ -1,105 +1,108 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 abstract class TextStyles {
-  static const String poppins = 'Poppins';
+  static const String family = 'Poppins';
   static const FontWeight bold = FontWeight.bold;
   static const FontWeight normal = FontWeight.normal;
 
-  static const TextStyle titleTextBold = TextStyle(
+  static final poppins = GoogleFonts.poppins();
+
+  static final TextStyle titleTextBold = poppins.copyWith(
     fontSize: 50,
     height: 75 / 50,
     fontWeight: bold,
-    fontFamily: poppins,
+    fontFamily: family,
   );
 
-  static const TextStyle headerTextBold = TextStyle(
+  static final TextStyle headerTextBold = poppins.copyWith(
     fontSize: 30,
     height: 45 / 30,
     fontWeight: bold,
-    fontFamily: poppins,
+    fontFamily: family,
   );
 
-  static const TextStyle largeTextBold = TextStyle(
+  static final TextStyle largeTextBold = poppins.copyWith(
     fontSize: 20,
     height: 30 / 20,
     fontWeight: bold,
-    fontFamily: poppins,
+    fontFamily: family,
   );
 
-  static const TextStyle mediumTextBold = TextStyle(
+  static final TextStyle mediumTextBold = poppins.copyWith(
     fontSize: 18,
     height: 27 / 18,
     fontWeight: bold,
-    fontFamily: poppins,
+    fontFamily: family,
   );
 
-  static const TextStyle normalTextBold = TextStyle(
+  static final TextStyle normalTextBold = poppins.copyWith(
     fontSize: 16,
     height: 24 / 16,
     fontWeight: bold,
-    fontFamily: poppins,
+    fontFamily: family,
   );
 
-  static const TextStyle smallTextBold = TextStyle(
+  static final TextStyle smallTextBold = poppins.copyWith(
     fontSize: 14,
     height: 21 / 14,
     fontWeight: bold,
-    fontFamily: poppins,
+    fontFamily: family,
   );
 
-  static const TextStyle smallerTextBold = TextStyle(
+  static final TextStyle smallerTextBold = poppins.copyWith(
     fontSize: 11,
     height: 17 / 11,
     fontWeight: bold,
-    fontFamily: poppins,
+    fontFamily: family,
   );
 
-  static const TextStyle titleTextRegular = TextStyle(
+  static final TextStyle titleTextRegular = poppins.copyWith(
     fontSize: 50,
     height: 75 / 50,
     fontWeight: normal,
-    fontFamily: poppins,
+    fontFamily: family,
   );
 
-  static const TextStyle headerTextRegular = TextStyle(
+  static final TextStyle headerTextRegular = poppins.copyWith(
     fontSize: 30,
     height: 45 / 30,
     fontWeight: normal,
-    fontFamily: poppins,
+    fontFamily: family,
   );
 
-  static const TextStyle largeTextRegular = TextStyle(
+  static final TextStyle largeTextRegular = poppins.copyWith(
     fontSize: 20,
     height: 30 / 20,
     fontWeight: normal,
-    fontFamily: poppins,
+    fontFamily: family,
   );
 
-  static const TextStyle mediumTextRegular = TextStyle(
+  static final TextStyle mediumTextRegular = poppins.copyWith(
     fontSize: 18,
     height: 27 / 18,
     fontWeight: normal,
-    fontFamily: poppins,
+    fontFamily: family,
   );
 
-  static const TextStyle normalTextRegular = TextStyle(
+  static final TextStyle normalTextRegular = poppins.copyWith(
     fontSize: 16,
     height: 24 / 16,
     fontWeight: normal,
-    fontFamily: poppins,
+    fontFamily: family,
   );
 
-  static const TextStyle smallTextRegular = TextStyle(
+  static final TextStyle smallTextRegular = poppins.copyWith(
     fontSize: 14,
     height: 21 / 14,
     fontWeight: normal,
-    fontFamily: poppins,
+    fontFamily: family,
   );
 
-  static const TextStyle smallerTextRegular = TextStyle(
+  static final TextStyle smallerTextRegular = poppins.copyWith(
     fontSize: 11,
     height: 27 / 11,
     fontWeight: normal,
-    fontFamily: poppins,
+    fontFamily: family,
   );
 }
