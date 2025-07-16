17d344f6908c9d1a5c860aa8ce29a9aaf7d253a8
ParkGiTeak <rl980901@naver.com>
2025-07-17 02:43:06 +0900

    feat: MainScreen FilterButton, RatingButton, RatingDialog 추가, BigButton, MediumButton, SmallButton 변경된 생성자 콜백 반영, 상태저장 변수 추가

--- sesac7_flutter_recipe_app/lib/presentation/main/main_screen.dart
+++ sesac7_flutter_recipe_app/lib/presentation/main/main_screen.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_recipe_app/core/enum/rating_type.dart';
+import 'package:flutter_recipe_app/core/enum/tap_state_type.dart';
 import 'package:flutter_recipe_app/data/model/label.dart';
 import 'package:flutter_recipe_app/presentation/component/button/big_button.dart';
+import 'package:flutter_recipe_app/presentation/component/button/filter_button.dart';
 import 'package:flutter_recipe_app/presentation/component/button/medium_button.dart';
+import 'package:flutter_recipe_app/presentation/component/button/rating_button.dart';
 import 'package:flutter_recipe_app/presentation/component/button/small_button.dart';
+import 'package:flutter_recipe_app/presentation/component/dialog/rating_dialog.dart';
 import 'package:flutter_recipe_app/presentation/component/tab/tabs.dart';
 import 'package:flutter_recipe_app/presentation/component/text_field/input_filed.dart';
 
 void main() {
-  runApp(const MainScreen());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MainScreen(),
+    );
+  }
 }
 
 class MainScreen extends StatefulWidget {
@@ -19,6 +39,12 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   int _selectedIndex = 0;
+  bool _ratingSelected = false;
+  bool _filterSelected = false;
+  bool _isBigButtonTabDown = false;
+  bool _isMediumButtonTabDown = false;
+  bool _isSmallButtonTabDown = false;
+  int _ratingDialogRating = 0;
 
   void _onTabChange(int selectedIndex) {
     setState(() {
@@ -32,36 +58,111 @@ class _MainScreenState extends State<MainScreen> {
       Label(labelTitle: 'Label1'),
       Label(labelTitle: 'Label2'),
     ];
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              SmallButton(title: 'Button'),
-              MediumButton(title: 'Button'),
-              BigButton(title: 'Button'),
-              InputFiled(
-                label: 'Label',
-                placeHolder: 'PlaceHolder',
-                value: 'value',
-                onValueChange: (value) {
-                  print(value);
-                },
-              ),
-              Tabs(
-                labels: labels,
-                selectedIndex: _selectedIndex,
-                onValueChange: (selectedIndex) {
-                  _onTabChange(selectedIndex);
-                },
-              ),
-            ],
-          ),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            RatingDialog(
+              dialogTitle: 'Rate recipe',
+              dialogButtonTitle: 'Send',
+              currentRatingType: _ratingDialogRating.intToRatingType(),
+              onChange: (rating) {
+                setState(() {
+                  _ratingDialogRating = rating;
+                });
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                RatingButton(
+                  ratingButtonTitle: RatingType.gradeFive.value,
+                  isSelected: _ratingSelected,
+                  onValueChange: (isSelected) {
+                    setState(() {
+                      _ratingSelected = isSelected;
+                    });
+                  },
+                ),
+                FilterButton(
+                  filterTitle: 'Text',
+                  isSelected: _filterSelected,
+                  onValueChange: (isSelected) {
+                    setState(() {
+                      _filterSelected = isSelected;
+                    });
+                  },
+                ),
+              ],
+            ),
+            SmallButton(
+              title: 'Button',
+              isTapDown: _isSmallButtonTabDown,
+              onTapStateChange: (tapState) {
+                setState(() {
+                  switch (tapState) {
+                    case TapStateType.onTapDown:
+                      _isSmallButtonTabDown = true;
+                      break;
+                    case TapStateType.onTapUp:
+                      _isSmallButtonTabDown = false;
+                    case TapStateType.onTapCancel:
+                      _isSmallButtonTabDown = false;
+                  }
+                });
+              },
+            ),
+            MediumButton(
+              title: 'Button',
+              isTapDown: _isMediumButtonTabDown,
+              onTapStateChange: (tapState) {
+                setState(() {
+                  switch (tapState) {
+                    case TapStateType.onTapDown:
+                      _isMediumButtonTabDown = true;
+                      break;
+                    case TapStateType.onTapUp:
+                      _isMediumButtonTabDown = false;
+                    case TapStateType.onTapCancel:
+                      _isMediumButtonTabDown = false;
+                  }
+                });
+              },
+            ),
+            BigButton(
+              title: 'Button',
+              isTapDown: _isBigButtonTabDown,
+              onTapStateChange: (tapState) {
+                setState(() {
+                  switch (tapState) {
+                    case TapStateType.onTapDown:
+                      _isBigButtonTabDown = true;
+                      break;
+                    case TapStateType.onTapUp:
+                      _isBigButtonTabDown = false;
+                    case TapStateType.onTapCancel:
+                      _isBigButtonTabDown = false;
+                  }
+                });
+              },
+            ),
+            InputFiled(
+              label: 'Label',
+              placeHolder: 'PlaceHolder',
+              value: 'value',
+              onValueChange: (value) {
+                print(value);
+              },
+            ),
+            Tabs(
+              labels: labels,
+              selectedIndex: _selectedIndex,
+              onValueChange: (selectedIndex) {
+                _onTabChange(selectedIndex);
+              },
+            ),
+          ],
         ),
       ),
     );
