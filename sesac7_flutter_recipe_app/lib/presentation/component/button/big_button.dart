366da18de3eb5601f88a9596761587fe8b500849
ParkGiTeak <rl980901@naver.com>
2025-07-16 22:09:03 +0900

    feat: onClick CallBack을 제거하고 TapStateType을 전달해주는 onTapStateChange CallBack 생성 후 GestureDetector에서 각 이벤트에 맞게 콜백호출해주도록 수정

--- sesac7_flutter_recipe_app/lib/presentation/component/button/big_button.dart
+++ sesac7_flutter_recipe_app/lib/presentation/component/button/big_button.dart
@@ -1,54 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_recipe_app/core/enum/tap_state_type.dart';
 import 'package:flutter_recipe_app/ui/app_colors.dart';
 import 'package:flutter_recipe_app/ui/text_styles.dart';
 
 class BigButton extends StatelessWidget {
   final String title;
-  final VoidCallback? onClick;
+  final bool isTapDown;
+  final Function(TapStateType) onTapStateChange;
 
   const BigButton({
     super.key,
     required this.title,
-    this.onClick,
+    required this.isTapDown,
+    required this.onTapStateChange,
   });
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        onClick?.call();
-      },
-      child: Container(
-        width: double.infinity,
-        height: 60.0,
-        decoration: BoxDecoration(
-          color: AppColors.primary100,
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        child: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: 114.0,
-                height: 24.0,
-                child: Center(
-                  child: Text(
-                    title,
-                    style: TextStyles.bigButtonTitle,
-                    maxLines: 1,
+    return AbsorbPointer(
+      absorbing: isTapDown,
+      child: GestureDetector(
+        onTapDown: (_) {
+          onTapStateChange(TapStateType.onTapDown);
+        },
+        onTapUp: (_) {
+          onTapStateChange(TapStateType.onTapUp);
+        },
+        onTapCancel: () {
+          onTapStateChange(TapStateType.onTapCancel);
+        },
+        child: Container(
+          width: double.infinity,
+          height: 60.0,
+          decoration: BoxDecoration(
+            color: isTapDown ? AppColors.gray4 : AppColors.primary100,
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 114.0,
+                  height: 24.0,
+                  child: Center(
+                    child: Text(
+                      title,
+                      style: TextStyles.bigButtonTitle,
+                      maxLines: 1,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                width: 11.0,
-              ),
-              SizedBox(
-                width: 20.0,
-                height: 20.0,
-                child: Icon(Icons.arrow_forward),
-              ),
-            ],
+                SizedBox(
+                  width: 11.0,
+                ),
+                SizedBox(
+                  width: 20.0,
+                  height: 20.0,
+                  child: Icon(Icons.arrow_forward),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- sesac7_flutter_recipe_app/lib/presentation/component/button/medium_button.dart
+++ sesac7_flutter_recipe_app/lib/presentation/component/button/medium_button.dart
@@ -1,28 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_recipe_app/core/enum/tap_state_type.dart';
 import 'package:flutter_recipe_app/ui/app_colors.dart';
 import 'package:flutter_recipe_app/ui/text_styles.dart';
 
 class MediumButton extends StatelessWidget {
   final String title;
-  final VoidCallback? onClick;
+  final bool isTapDown;
+  final Function(TapStateType) onTapStateChange;
 
   const MediumButton({
     super.key,
     required this.title,
-    this.onClick,
+    required this.isTapDown,
+    required this.onTapStateChange,
   });
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        onClick?.call();
+      onTapDown: (_) {
+        onTapStateChange(TapStateType.onTapDown);
+      },
+      onTapUp: (_) {
+        onTapStateChange(TapStateType.onTapUp);
+      },
+      onTapCancel: () {
+        onTapStateChange(TapStateType.onTapCancel);
       },
       child: Container(
         width: 243.0,
         height: 54.0,
         decoration: BoxDecoration(
-          color: AppColors.primary100,
+          color: isTapDown ? AppColors.gray4 : AppColors.primary100,
           borderRadius: BorderRadius.circular(10.0),
         ),
         child: Center(
