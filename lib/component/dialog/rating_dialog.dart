27060ac9b4616b359795593d32326ead74b574e7
0h9u <kog6148@gmail.com>
2025-07-17 03:27:30 +0900

    feat : rating dialog 기능 추가

---
+++ lib/component/dialog/rating_dialog.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_recipe_app/component/button/small_button.dart';
+import 'package:flutter_recipe_app/ui/app_color.dart';
+
+class RatingDialog extends StatefulWidget {
+  final String title;
+  final String actionName;
+  final ValueChanged<int> onRatingChanged;
+
+  const RatingDialog({
+    super.key,
+    this.title = 'Rate recipe',
+    this.actionName = 'send',
+    required this.onRatingChanged,
+  });
+
+  @override
+  State<RatingDialog> createState() => _RatingDialogState();
+}
+
+class _RatingDialogState extends State<RatingDialog> {
+  int _selectedRating = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      title: Center(
+        child: Text(
+          widget.title,
+          style: const TextStyle(
+            fontSize: 17,
+            color: AppColor.Black,
+          ),
+        ),
+      ),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: List.generate(5, (index) {
+              final ratingValue = index + 1;
+              return GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _selectedRating = ratingValue;
+                  });
+                },
+                child: Icon(
+                  ratingValue <= _selectedRating
+                      ? Icons.star
+                      : Icons.star_border_outlined,
+                  color: AppColor.Rating,
+                  size: 32,
+                ),
+              );
+            }),
+          ),
+          SizedBox(height: 5),
+          SmallButton(
+            buttonText: widget.actionName,
+            buttonColor: AppColor.Rating,
+            onTap: _selectedRating > 0
+                ? () {
+                    widget.onRatingChanged(_selectedRating);
+                    Navigator.of(context).pop();
+                  }
+                : null,
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_recipe_app/component/button/big_button.dart';
+import 'package:flutter_recipe_app/component/button/filter_button.dart';
 import 'package:flutter_recipe_app/component/button/rating_button.dart';
+import 'package:flutter_recipe_app/component/button/small_button.dart';
+import 'package:flutter_recipe_app/component/dialog/rating_dialog.dart';
 import 'package:flutter_recipe_app/component/widget/ingredient_item.dart';
 import 'package:flutter_recipe_app/component/widget/recipe_thumbnail.dart';
 import 'package:google_fonts/google_fonts.dart';
-
-import 'component/button/filter_button.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,47 +23,71 @@ class MyApp extends StatelessWidget {
         textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
         scaffoldBackgroundColor: Colors.white,
       ),
-      home: Scaffold(
-        body: Center(
-          child: SingleChildScrollView(
-            padding: EdgeInsets.symmetric(horizontal: 30),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                BigButton(
-                  buttonText: 'Button',
-                  onTap: () {
-                    print('Big Button');
-                  },
-                ),
-                SizedBox(height: 10),
-                IngredientItem(onTap: () => print('ingredient Item')),
-                SizedBox(height: 10),
-                RecipeThumbnail(
-                  userName: 'Chef John',
-                  firstLine: 'Traditional spare ribs',
-                  secondLine: 'baked',
-                  rating: 4.0,
-                  minutes: 20,
-                  onTapList: () => print('Recipe Thumbnail'),
-                  onTapBookmark: () => print('Bookmark'),
-                ),
-                SizedBox(height: 10),
-                Row(
-                  children: [
-                    RatingButton(
-                      rating: 5,
-                      onTap: () => print('Rating Button'),
-                    ),
-                    SizedBox(width: 5),
-                    FilterButton(onTap: () => print('Filter Button')),
-                  ],
-                ),
-              ],
-            ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              BigButton(
+                buttonText: 'Button',
+                onTap: () {
+                  print('Big Button');
+                },
+              ),
+              const SizedBox(height: 10),
+              IngredientItem(onTap: () => print('ingredient Item')),
+              const SizedBox(height: 10),
+              RecipeThumbnail(
+                userName: 'Chef John',
+                firstLine: 'Traditional spare ribs',
+                secondLine: 'baked',
+                rating: 4.0,
+                minutes: 20,
+                onTapList: () => print('Recipe Thumbnail'),
+                onTapBookmark: () => print('Bookmark'),
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  RatingButton(rating: 5, onTap: () => print('Rating Button')),
+                  const SizedBox(width: 5),
+                  FilterButton(onTap: () => print('Filter Button')),
+                ],
+              ),
+              const SizedBox(height: 10),
+              SmallButton(
+                buttonText: 'Rating',
+                onTap: () => showRatingDialog(context),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
+}
+
+void showRatingDialog(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return RatingDialog(
+        onRatingChanged: (rating) {
+          print('rating : $rating');
+        },
+      );
+    },
+  );
 }
