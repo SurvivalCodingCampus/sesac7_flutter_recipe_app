eaa1890e0bcfa39c6953d41d85ecf2e568b489b9
jinaoh <jina7430@gmail.com>
2025-07-17 00:20:43 +0900

    feat: recipe card 작성 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_recipe_app/presentation/screen/ingredient_screen.dart';
+import 'package:flutter_recipe_app/presentation/screen/recipe_card_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         textTheme: GoogleFonts.poppinsTextTheme(),
       ),
 
-      home: const IngredientScreen(),
+      home: const RecipeCardScreen(),
     );
   }
 }
