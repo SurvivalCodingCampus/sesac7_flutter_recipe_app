61f61f94e0548960f33b6878c4fc40d5f770ea41
Moon <1tjsen@naver.com>
2025-07-17 19:01:30 +0900

    feat: RecipeDataSource 추가

---
+++ lib/data/data_source/recipe/recipe_data_source.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_recipe_app/core/response.dart';
+import 'package:flutter_recipe_app/data/dto/recipe/recipe_dto.dart';
+
+abstract interface class RecipeDataSource {
+  Future<Response<List<RecipeDto>>> fetchRecipes();
+}
