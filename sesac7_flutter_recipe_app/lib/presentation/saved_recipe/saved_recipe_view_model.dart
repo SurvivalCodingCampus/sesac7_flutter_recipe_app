dc3f5728a749e2f635da2646b9ceb34d03aa0353
ParkGiTeak <rl980901@naver.com>
2025-07-17 23:17:40 +0900

    feat: SavedRecipeViewModel Class 추가

---
+++ sesac7_flutter_recipe_app/lib/presentation/saved_recipe/saved_recipe_view_model.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_recipe_app/core/enum/network_error.dart';
+import 'package:flutter_recipe_app/core/result.dart';
+import 'package:flutter_recipe_app/data/model/recipe.dart';
+import 'package:flutter_recipe_app/data/repository/recipe_repository/recipe_repository.dart';
+
+class SavedRecipeViewModel with ChangeNotifier {
+  final RecipeRepository _recipeRepository;
+  List<Recipe> _recipes = [];
+
+  List<Recipe> get recipes => _recipes;
+
+  SavedRecipeViewModel(RecipeRepository recipeRepository)
+    : _recipeRepository = recipeRepository;
+
+  Future<void> fetchRecipes() async {
+    final Result<List<Recipe>, NetworkError> result = await _recipeRepository.getRecipes();
+    switch (result) {
+      case Success():
+        _recipes = result.data;
+        break;
+      case Error():
+        _recipes = [];
+        // todo 에러 처리
+        break;
+    }
+  }
+}
