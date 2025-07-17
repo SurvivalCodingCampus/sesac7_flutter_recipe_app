3b66e5da571250bdf8fa71f6292d63fd8f3d42b4
Hagmin.Lee <iilhm2001@naver.com>
2025-07-17 09:44:13 +0900

    feat: IngredientItem 컴포넌트 생성

---
+++ lib/02_stateful_widget/presentation/component/ingredient_item.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_recipe_app/01_stateless_widget/ui/app_colors.dart';
+import 'package:flutter_recipe_app/01_stateless_widget/ui/text_styles.dart';
+import 'package:flutter_recipe_app/02_stateful_widget/data/model/ingredient.dart';
+
+class IngredientItem extends StatelessWidget {
+  final Ingredient ingredient;
+  final int quantity;
+
+  const IngredientItem({
+    super.key,
+    required this.ingredient,
+    required this.quantity,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 76,
+      margin: EdgeInsets.fromLTRB(30, 0, 30, 0),
+      decoration: BoxDecoration(
+        color: AppColors.gray4,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 52,
+            height: 52,
+            margin: EdgeInsets.fromLTRB(15, 12, 0, 12),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(ingredient.image),
+              ),
+              // color: Colors.red,
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          SizedBox(
+            width: 16,
+            height: 24,
+          ),
+          Expanded(
+            child: Container(
+              // width: 73,
+              margin: EdgeInsets.fromLTRB(0, 29, 0, 23),
+              height: 24,
+              child: Text(
+                ingredient.name,
+                style: TextStyles.normalTextBold,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 108,
+            height: 24,
+          ),
+          Container(
+            height: 21,
+            margin: EdgeInsets.fromLTRB(0, 28, 15, 27),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  '${quantity}g',
+                  style: TextStyles.smallTextRegular.copyWith(
+                    color: AppColors.gray3,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
