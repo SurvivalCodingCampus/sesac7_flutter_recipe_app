f6efc22ef75056075abb866cd375a29e1cabfe4c
Moon <1tjsen@naver.com>
2025-07-17 19:07:43 +0900

    refactor: RecipeDataSource fetchAllRecipes 네이밍 수정

--- lib/data/data_source/recipe/recipe_data_source.dart
+++ lib/data/data_source/recipe/recipe_data_source.dart
@@ -2,5 +2,5 @@ import 'package:flutter_recipe_app/core/response.dart';
 import 'package:flutter_recipe_app/data/dto/recipe/recipe_dto.dart';
 
 abstract interface class RecipeDataSource {
-  Future<Response<List<RecipeDto>>> fetchRecipes();
+  Future<Response<List<RecipeDto>>> fetchAllRecipes();
 }
