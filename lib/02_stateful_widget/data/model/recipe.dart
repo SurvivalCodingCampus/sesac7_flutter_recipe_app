179a9ac8799082441b178a2401b93208caae7d9f
Hagmin.Lee <iilhm2001@naver.com>
2025-07-17 09:45:26 +0900

    feat: Recipe 모델 생성

---
+++ lib/02_stateful_widget/data/model/recipe.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_recipe_app/02_stateful_widget/data/model/ingredient.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'recipe.freezed.dart';
+part 'recipe.g.dart';
+
+@freezed
+abstract class Recipe with _$Recipe {
+  const factory Recipe({
+    required String category,
+    required int id,
+    required String name,
+    required String image,
+    required String chef,
+    required String cookingTime,
+    required double rating,
+    required List<Ingredient> ingredients,
+  }) = _Recipe;
+
+  factory Recipe.fromJson(Map<String, Object?> json) => _$RecipeFromJson(json);
+}
