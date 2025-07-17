945315424badd5f192afb0685cd86fde7a336db3
ParkGiTeak <rl980901@naver.com>
2025-07-17 23:13:25 +0900

    feat: Recipe Mapper toModel return Ingredients 인자 부분 수정, toDto extension 추가

--- sesac7_flutter_recipe_app/lib/data/mapper/recipe_mapper.dart
+++ sesac7_flutter_recipe_app/lib/data/mapper/recipe_mapper.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_recipe_app/data/dto/recipe_dto.dart';
-import 'package:flutter_recipe_app/data/model/ingredient.dart';
+import 'package:flutter_recipe_app/data/mapper/ingredients_mapper.dart';
+import 'package:flutter_recipe_app/data/model/ingredients.dart';
 import 'package:flutter_recipe_app/data/model/recipe.dart';
 
 extension RecipeDtoToModel on RecipeDto {
@@ -12,9 +13,24 @@ extension RecipeDtoToModel on RecipeDto {
       chef: chef!,
       time: time!,
       rating: rating!.toDouble(),
-      ingredients:
-          ingredients?.map((ingredient) => ingredient as Ingredient).toList() ??
-          [],
+      ingredients: ingredients?.map((dto) => dto.toModel()).toList() ?? [],
+    );
+  }
+}
+
+extension RecipeModelToDto on Recipe {
+  RecipeDto toDto() {
+    return RecipeDto(
+      category: category,
+      id: id.toInt(),
+      name: name,
+      image: image,
+      chef: chef,
+      time: time,
+      rating: rating.toDouble(),
+      ingredients: ingredients
+          .map((model) => (model as Ingredients).toDto())
+          .toList(),
     );
   }
 }
