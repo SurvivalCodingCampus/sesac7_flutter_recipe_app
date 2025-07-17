dd24ae0886976984f1afb0d4a5c60375e1389a56
Hagmin.Lee <iilhm2001@naver.com>
2025-07-17 09:47:37 +0900

    feat: RatingButton 테스트 코드 작성

---
+++ test/02_stateful_widget/presentation/component/unclicked_rating_button_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_recipe_app/02_stateful_widget/presentation/component/unclicked_rating_button.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('UnclickedRatingButton renders correctly', (tester) async {
+    // given
+    final testText = '4';
+    final testIsSelected = false;
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: UnclickedRatingButton(text: testText, isSelected: testIsSelected),
+      ),
+    );
+
+    // then
+    expect(find.byType(UnclickedRatingButton), findsOneWidget);
+    expect(find.text(testText), findsOneWidget);
+    expect(find.byIcon(Icons.star), findsOneWidget);
+  });
+}
