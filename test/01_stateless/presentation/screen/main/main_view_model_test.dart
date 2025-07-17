105a1aa26cfde336419b05f9b79dd155c46d2afd
Junsuk Oh <junsuk.oh@survivalcoding.com>
2025-07-17 17:52:12 +0900

    test: MainViewModel 유닛 테스트 추가
    - `incrementCount` 메서드 테스트: 카운트가 올바르게 증가하는지 확인합니다.
    - `setScore` 메서드 테스트: 점수가 올바르게 설정되는지 확인합니다.
    - `fetchPersonData` 메서드 그룹 테스트:
        - 로딩 상태가 `true`에서 `false`로 올바르게 변경되는지 확인합니다.
        - `PersonRepository`로부터 이름과 나이를 성공적으로 가져오는지 확인합니다.
        - 로딩 상태 변경 시 리스너에게 알림이 가는지 확인합니다.

---
+++ test/01_stateless/presentation/screen/main/main_view_model_test.dart
@@ -0,0 +1,70 @@
+import 'package:flutter_recipe_app/01_stateless/data/repository/person_repository.dart';
+import 'package:flutter_recipe_app/01_stateless/presentation/screen/main/main_view_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'main_view_model_test.mocks.dart';
+
+@GenerateMocks([PersonRepository])
+void main() {
+  late MainViewModel viewModel;
+  late PersonRepository personRepository;
+
+  setUp(() {
+    personRepository = MockPersonRepository();
+    viewModel = MainViewModel(personRepository: personRepository);
+  });
+
+  test('카운트가 1 증가해야 한다', () {
+    viewModel.incrementCount();
+    expect(viewModel.count, 1);
+  });
+
+  test('점수가 설정되어야 한다', () {
+    const score = 99;
+    viewModel.setScore(score);
+    expect(viewModel.score, score);
+  });
+
+  group('사용자 정보 가져오기', () {
+    test('로딩 상태가 true에서 false로 변경되어야 한다', () async {
+      when(personRepository.getName()).thenAnswer((_) async => '홍길동');
+      when(personRepository.getAge()).thenAnswer((_) async => 30);
+
+      final future = viewModel.fetchPersonData();
+
+      expect(viewModel.isLoading, isTrue);
+
+      await future;
+
+      expect(viewModel.isLoading, isFalse);
+    });
+
+    test('이름과 나이를 가져와야 한다', () async {
+      const name = '홍길동';
+      const age = 30;
+      when(personRepository.getName()).thenAnswer((_) async => name);
+      when(personRepository.getAge()).thenAnswer((_) async => age);
+
+      await viewModel.fetchPersonData();
+
+      expect(viewModel.name, name);
+      expect(viewModel.age, age);
+    });
+
+    test('로딩 상태가 변경될 때 리스너에게 알려야 한다', () async {
+      when(personRepository.getName()).thenAnswer((_) async => '홍길동');
+      when(personRepository.getAge()).thenAnswer((_) async => 30);
+
+      final listenerCallCount = <int>[];
+      viewModel.addListener(() {
+        listenerCallCount.add(viewModel.isLoading ? 1 : 0);
+      });
+
+      await viewModel.fetchPersonData();
+
+      expect(listenerCallCount, [1, 0]);
+    });
+  });
+}
