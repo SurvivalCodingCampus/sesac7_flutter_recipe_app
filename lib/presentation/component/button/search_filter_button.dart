335811b2b2af6633dc3a1aa48c8584ad25b9466d
Moon <1tjsen@naver.com>
2025-07-18 22:44:42 +0900

    fix: SearchFilterButton에 onTap 추가

--- lib/presentation/component/button/search_filter_button.dart
+++ lib/presentation/component/button/search_filter_button.dart
@@ -5,21 +5,29 @@ import 'package:flutter_recipe_app/ui/app_colors.dart';
 class SearchFilterButton extends StatelessWidget {
   static const double size = 40;
 
-  const SearchFilterButton({super.key});
+  final VoidCallback onTap;
+
+  const SearchFilterButton({
+    super.key,
+    required this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: size,
-      height: size,
-      decoration: BoxDecoration(
-        color: AppColors.primary100,
-        borderRadius: BorderRadius.circular(ComponentConstant.borderRadius),
-      ),
-      child: Icon(
-        Icons.format_line_spacing_outlined,
-        color: AppColors.white,
-        size: ComponentConstant.iconSize,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: size,
+        height: size,
+        decoration: BoxDecoration(
+          color: AppColors.primary100,
+          borderRadius: BorderRadius.circular(ComponentConstant.borderRadius),
+        ),
+        child: Icon(
+          Icons.format_line_spacing_outlined,
+          color: AppColors.white,
+          size: ComponentConstant.iconSize,
+        ),
       ),
     );
   }
