226d8f6204be35fa1b5780f747ba4db51d9e9ae9
Moon <1tjsen@naver.com>
2025-07-19 19:31:20 +0900

    refactor: RecipeRepository 불필요한 메서드 제거

--- lib/data/repository/recipe/recipe_repository.dart
+++ lib/data/repository/recipe/recipe_repository.dart
@@ -1,17 +1,7 @@
 import 'package:flutter_recipe_app/core/network_error.dart';
 import 'package:flutter_recipe_app/core/result.dart';
-import 'package:flutter_recipe_app/data/model/recipe/filter_category.dart';
-import 'package:flutter_recipe_app/data/model/recipe/filter_rate.dart';
-import 'package:flutter_recipe_app/data/model/recipe/filter_sort_by.dart';
 import 'package:flutter_recipe_app/data/model/recipe/recipe.dart';
 
 abstract interface class RecipeRepository {
   Future<Result<List<Recipe>, NetworkError>> fetchAllRecipes();
-  Future<Result<List<Recipe>, NetworkError>> searchRecipes(String keyword);
-  Future<Result<List<Recipe>, NetworkError>> filterRecipes({
-    required String keyword,
-    FilterSortBy? filterSortBy,
-    FilterRate? filterRate,
-    FilterCategory? filterCategory,
-  });
 }
--- lib/data/repository/recipe/recipe_repository_impl.dart
+++ lib/data/repository/recipe/recipe_repository_impl.dart
@@ -5,9 +5,6 @@ import 'package:flutter_recipe_app/core/network_validator_mixin.dart';
 import 'package:flutter_recipe_app/core/result.dart';
 import 'package:flutter_recipe_app/data/data_source/recipe/recipe_data_source.dart';
 import 'package:flutter_recipe_app/data/mapper/recipe/recipe_mapper.dart';
-import 'package:flutter_recipe_app/data/model/recipe/filter_category.dart';
-import 'package:flutter_recipe_app/data/model/recipe/filter_rate.dart';
-import 'package:flutter_recipe_app/data/model/recipe/filter_sort_by.dart';
 import 'package:flutter_recipe_app/data/model/recipe/recipe.dart';
 import 'package:flutter_recipe_app/data/repository/recipe/recipe_repository.dart';
 
@@ -42,95 +39,5 @@ class RecipeRepositoryImpl
     } catch (e) {
       return Result.error(NetworkError.unknown);
     }
-  }
-
-  @override
-  Future<Result<List<Recipe>, NetworkError>> filterRecipes({
-    required String keyword,
-    FilterSortBy? filterSortBy,
-    FilterRate? filterRate,
-    FilterCategory? filterCategory,
-  }) async {
-    final result = await searchRecipes(keyword);
-
-    switch (result) {
-      case Success<List<Recipe>, NetworkError>():
-        var recipes = result.data;
-
-        if (filterSortBy != null) {
-          recipes = _filterRecipesSortedBy(recipes, filterSortBy);
-        }
-        if (filterRate != null) {
-          recipes = _filterRecipesByRate(recipes, filterRate);
-        }
-        if (filterCategory != null) {
-          recipes = _filterRecipesByCategory(recipes, filterCategory);
-        }
-
-        return Result.success(recipes);
-      case Error<List<Recipe>, NetworkError>():
-        return Result.error(result.error);
-    }
-  }
-
-  @override
-  Future<Result<List<Recipe>, NetworkError>> searchRecipes(
-    String keyword,
-  ) async {
-    final result = await fetchAllRecipes();
-
-    switch (result) {
-      case Success<List<Recipe>, NetworkError>():
-        return Result.success(
-          result.data
-              .where(
-                (e) => e.name.contains(keyword) || e.creator.contains(keyword),
-              )
-              .toList(),
-        );
-      case Error<List<Recipe>, NetworkError>():
-        return Result.error(result.error);
-    }
-  }
-
-  List<Recipe> _filterRecipesSortedBy(
-    List<Recipe> recipes,
-    FilterSortBy sortBy,
-  ) {
-    return switch (sortBy) {
-      FilterSortBy.all => recipes,
-      FilterSortBy.newest => recipes,
-      FilterSortBy.oldest => recipes,
-      FilterSortBy.popularity => recipes,
-    };
-  }
-
-  List<Recipe> _filterRecipesByRate(
-    List<Recipe> recipes,
-    FilterRate filterRate,
-  ) {
-    return switch (filterRate) {
-      FilterRate.one => recipes.where((e) => e.rating < 2).toList(),
-      FilterRate.two =>
-        recipes.where((e) => e.rating >= 2 && e.rating < 3).toList(),
-      FilterRate.three =>
-        recipes.where((e) => e.rating >= 3 && e.rating < 4).toList(),
-      FilterRate.four =>
-        recipes.where((e) => e.rating >= 4 && e.rating < 5).toList(),
-      FilterRate.five => recipes.where((e) => e.rating >= 5).toList(),
-    };
-  }
-
-  List<Recipe> _filterRecipesByCategory(
-    List<Recipe> recipes,
-    FilterCategory filterCategory,
-  ) {
-    if (filterCategory == FilterCategory.all) {
-      return recipes;
-    }
-
-    return recipes
-        .where((e) => e.category == filterCategory.toString())
-        .toList();
   }
 }
