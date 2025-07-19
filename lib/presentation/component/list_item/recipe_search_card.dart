225fb9087e42a4caa95e13354e01b4354e956d09
Moon <1tjsen@naver.com>
2025-07-19 20:12:21 +0900

    feat: RecipeSearchCard 추가

--- lib/presentation/component/list_item/recipe_search_card.dart
+++ lib/presentation/component/list_item/recipe_search_card.dart
@@ -4,33 +4,24 @@ import 'package:flutter_recipe_app/presentation/component/constants/component_co
 import 'package:flutter_recipe_app/ui/app_colors.dart';
 import 'package:flutter_recipe_app/ui/text_styles.dart';
 
-class RecipeCard extends StatelessWidget {
-  static const double width = 315;
-  static const double height = 150;
+class RecipeSearchCard extends StatelessWidget {
   static const double positionedPadding = 10;
   static const double ratingBoxWidth = 37;
   static const double ratingBoxHeight = 16;
   static const double ratingBoxBorderRadius = 20;
   static const double ratingIconSize = 8;
   static const double smallerTextSize = 8;
-  static const double timerIconSize = 17;
-  static const double bookmarkButtonSize = 24;
-  static const double bookmarkIconSize = 13;
 
   final Recipe recipe;
-  final VoidCallback onBookmarkTap;
 
-  const RecipeCard({
+  const RecipeSearchCard({
     super.key,
     required this.recipe,
-    required this.onBookmarkTap,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: width,
-      height: height,
       decoration: BoxDecoration(
         image: DecorationImage(
           image: Image.network(recipe.imageUrl).image,
@@ -91,71 +82,24 @@ class RecipeCard extends StatelessWidget {
             bottom: positionedPadding,
             left: positionedPadding,
             right: positionedPadding,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.end,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisSize: MainAxisSize.min,
               children: [
-                Flexible(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
-                        recipe.name,
-                        style: TextStyles.smallTextBold.copyWith(
-                          color: AppColors.white,
-                        ),
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      Text(
-                        'By ${recipe.creator}',
-                        style: TextStyles.smallerTextRegular.copyWith(
-                          fontSize: smallerTextSize,
-                          color: AppColors.gray4,
-                        ),
-                      ),
-                    ],
+                Text(
+                  recipe.name,
+                  style: TextStyles.smallerTextBold.copyWith(
+                    color: AppColors.white,
                   ),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
                 ),
-                Row(
-                  spacing: 10,
-                  children: [
-                    Row(
-                      spacing: 5,
-                      children: [
-                        Icon(
-                          Icons.timer_outlined,
-                          size: timerIconSize,
-                          color: AppColors.gray4,
-                        ),
-                        Text(
-                          recipe.cookingTime,
-                          style: TextStyles.smallerTextRegular.copyWith(
-                            color: AppColors.gray4,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Material(
-                      color: AppColors.white,
-                      shape: const CircleBorder(),
-                      child: InkWell(
-                        onTap: onBookmarkTap,
-                        customBorder: const CircleBorder(),
-                        child: SizedBox(
-                          width: bookmarkButtonSize,
-                          height: bookmarkButtonSize,
-                          child: Icon(
-                            Icons.bookmark_outline,
-                            size: bookmarkIconSize,
-                            color: AppColors.primary80,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
+                Text(
+                  'By ${recipe.creator}',
+                  style: TextStyles.smallerTextRegular.copyWith(
+                    fontSize: smallerTextSize,
+                    color: AppColors.gray4,
+                  ),
                 ),
               ],
             ),
