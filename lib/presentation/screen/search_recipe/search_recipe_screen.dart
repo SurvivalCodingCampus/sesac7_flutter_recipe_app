dc8ec9fd7225acf799d3c61048e319fc33e5f953
Moon <1tjsen@naver.com>
2025-07-19 20:37:01 +0900

    feat: SearchRecipeScreen 추가

---
+++ lib/presentation/screen/search_recipe/search_recipe_screen.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_recipe_app/data/data_source/recipe/recipe_data_source_impl.dart';
+import 'package:flutter_recipe_app/data/repository/recipe/recipe_repository_impl.dart';
+import 'package:flutter_recipe_app/presentation/component/button/search_filter_button.dart';
+import 'package:flutter_recipe_app/presentation/component/input/search_field.dart';
+import 'package:flutter_recipe_app/presentation/component/list_item/recipe_search_card.dart';
+import 'package:flutter_recipe_app/presentation/screen/search_recipe/search_recipe_state.dart';
+import 'package:flutter_recipe_app/presentation/screen/search_recipe/search_recipe_view_model.dart';
+import 'package:flutter_recipe_app/ui/app_colors.dart';
+import 'package:flutter_recipe_app/ui/text_styles.dart';
+
+class SearchRecipeScreen extends StatelessWidget {
+  final SearchRecipeViewModel viewModel;
+
+  const SearchRecipeScreen({
+    super.key,
+    required this.viewModel,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final recipes = viewModel.state.filteredRecipes;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Search recipes',
+          style: TextStyles.mediumTextBold,
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            spacing: 20,
+            children: [
+              Row(
+                spacing: 20,
+                children: [
+                  Expanded(
+                    child: SearchField(
+                      placeholder: 'Search recipe',
+                      onValueChange: viewModel.searchRecipe,
+                    ),
+                  ),
+                  SearchFilterButton(onTap: () {}),
+                ],
+              ),
+              Row(
+                children: [
+                  Text(
+                    viewModel.state.searchState,
+                    style: TextStyles.normalTextBold,
+                  ),
+                  const Spacer(),
+                  if (viewModel.state.searchState ==
+                      SearchRecipeState.searchResult)
+                    Text(
+                      '${viewModel.state.resultCount} results',
+                      style: TextStyles.smallerTextRegular.copyWith(
+                        color: AppColors.gray3,
+                      ),
+                    ),
+                ],
+              ),
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 15,
+                    crossAxisSpacing: 15,
+                  ),
+                  itemCount: recipes.length,
+                  itemBuilder: (context, index) {
+                    return RecipeSearchCard(recipe: recipes[index]);
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() async {
+  final viewModel = SearchRecipeViewModel(
+    recipeRepository: RecipeRepositoryImpl(
+      recipeDataSource: RecipeDataSourceImpl(),
+    ),
+  );
+  await viewModel.fetchRecipe();
+  runApp(
+    MaterialApp(
+      home: ListenableBuilder(
+        listenable: viewModel,
+        builder: (context, child) {
+          return SearchRecipeScreen(viewModel: viewModel);
+        },
+      ),
+    ),
+  );
+}
