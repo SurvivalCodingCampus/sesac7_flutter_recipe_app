097a952e07a420e75c8a1f7d3d0b356fccc8a6e1
Hagmin.Lee <iilhm2001@naver.com>
2025-07-19 16:24:50 +0900

    fix: 배경 그림 gradient 추가

--- lib/02_stateful_widget/presentation/component/recipe_card.dart
+++ lib/02_stateful_widget/presentation/component/recipe_card.dart
@@ -18,9 +18,18 @@ class RecipeCard extends StatelessWidget {
     return Container(
       // width: 315,
       height: 150,
-      padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+      // padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
       margin: EdgeInsets.fromLTRB(30, 0, 30, 0),
       decoration: BoxDecoration(
+        // gradient: LinearGradient(
+        //   begin: Alignment.topCenter,
+        //   end: Alignment.bottomCenter,
+        //   colors: [
+        //     Colors.white,
+        //     AppColors.black,
+        //   ],
+        //   stops: [0.0, 0.8],
+        // ),
         boxShadow: [
           BoxShadow(
             color: Colors.black.withValues(),
@@ -29,121 +38,157 @@ class RecipeCard extends StatelessWidget {
             offset: Offset(0, 2),
           ),
         ],
-        image: DecorationImage(
-          fit: BoxFit.cover,
-          image: NetworkImage(recipe.image),
-        ),
+        // images: DecorationImage(
+        //   fit: BoxFit.cover,
+        //   images: NetworkImage(recipe.images),
+        // ),
         borderRadius: BorderRadius.circular(10),
       ),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Expanded(child: SizedBox()),
-              Container(
-                width: 37,
-                height: 16,
-                // padding: EdgeInsets.fromLTRB(7, 2, 7, 2),
-                // margin: EdgeInsets.only(left: 255),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Container(
                 decoration: BoxDecoration(
-                  color: AppColors.secondary20,
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.star, color: AppColors.rating, size: 7),
-                    SizedBox(width: 3),
-                    Text(
-                      '${recipe.rating}',
-                      style: TextStyles.smallTextRegular.copyWith(
-                        fontSize: 8,
-                      ),
-                    ),
-                  ],
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(recipe.image),
+                  ),
                 ),
               ),
-            ],
-          ),
-          SizedBox(
-            height: 60,
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      child: Text(
-                        recipe.name,
-                        style: TextStyles.smallTextBold.copyWith(
-                          color: AppColors.white,
+            ),
+            Positioned.fill(
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Colors.black.withValues(alpha: 0.2),
+                      AppColors.black.withValues(alpha: 0.5),
+                    ],
+                    stops: [0.5, 1.0],
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(child: SizedBox()),
+                      Container(
+                        width: 37,
+                        height: 16,
+                        // padding: EdgeInsets.fromLTRB(7, 2, 7, 2),
+                        // margin: EdgeInsets.only(left: 255),
+                        decoration: BoxDecoration(
+                          color: AppColors.secondary20,
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.star, color: AppColors.rating, size: 7),
+                            SizedBox(width: 3),
+                            Text(
+                              '${recipe.rating}',
+                              style: TextStyles.smallTextRegular.copyWith(
+                                fontSize: 8,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    ),
-                    Text(
-                      'By ${recipe.chef}',
-                      style: TextStyles.smallTextRegular.copyWith(
-                        fontSize: 8,
-                        color: AppColors.gray4,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Column(
-                children: [
+                    ],
+                  ),
                   SizedBox(
-                    width: 94,
-                    height: 30,
+                    height: 60,
                   ),
                   Row(
                     children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.watch_later_outlined,
-                            color: AppColors.gray4,
-                            size: 17,
-                          ),
-                          SizedBox(
-                            width: 5,
-                          ),
-                          Text(
-                            recipe.cookingTime,
-                            style: TextStyles.smallTextRegular.copyWith(
-                              color: AppColors.gray4,
-                              fontSize: 11.0,
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              child: Text(
+                                recipe.name,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyles.smallTextBold.copyWith(
+                                  color: AppColors.white,
+                                ),
+                              ),
                             ),
+                            Text(
+                              'By ${recipe.chef}',
+                              style: TextStyles.smallTextRegular.copyWith(
+                                fontSize: 8,
+                                color: AppColors.gray4,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          SizedBox(
+                            width: 94,
+                            height: 30,
+                          ),
+                          Row(
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    Icons.watch_later_outlined,
+                                    color: AppColors.gray4,
+                                    size: 17,
+                                  ),
+                                  SizedBox(
+                                    width: 5,
+                                  ),
+                                  Text(
+                                    recipe.cookingTime,
+                                    style: TextStyles.smallTextRegular.copyWith(
+                                      color: AppColors.gray4,
+                                      fontSize: 11.0,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(width: 10, height: 24),
+                              GestureDetector(
+                                onTap: onClick,
+                                child: Container(
+                                  width: 24,
+                                  height: 24,
+                                  decoration: BoxDecoration(
+                                    color: AppColors.white,
+                                    borderRadius: BorderRadius.circular(100),
+                                  ),
+                                  child: Icon(
+                                    Icons.bookmark_border,
+                                    size: 16,
+                                    color: AppColors.primary80,
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ],
-                      ),
-                      SizedBox(width: 10, height: 24),
-                      GestureDetector(
-                        onTap: onClick,
-                        child: Container(
-                          width: 24,
-                          height: 24,
-                          decoration: BoxDecoration(
-                            color: AppColors.white,
-                            borderRadius: BorderRadius.circular(100),
-                          ),
-                          child: Icon(
-                            Icons.bookmark_border,
-                            size: 16,
-                            color: AppColors.primary80,
-                          ),
-                        ),
                       ),
                     ],
                   ),
                 ],
               ),
-            ],
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
