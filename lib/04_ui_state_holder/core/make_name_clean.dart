244a7278f688d75af6ae8d1cd9b79b9d9db0a27e
Hagmin.Lee <iilhm2001@naver.com>
2025-07-21 02:30:35 +0900

    refactor: MakeNameClean extension 리팩토링

--- lib/04_ui_state_holder/core/make_name_clean.dart
+++ lib/04_ui_state_holder/core/make_name_clean.dart
@@ -1,56 +1,27 @@
 import 'package:flutter_recipe_app/04_ui_state_holder/presentation/screen/search_recipes_screen.dart';
 
 extension MakeNameClean on SearchRecipesScreen {
-  String getRecipeName(int index) {
-    final List<String> listOfName = viewModel.state.recipes[index].name.split(
-      ' ',
-    );
-    final List<String> result = [];
+  String _formatRecipeName(String name) {
+    final List<String> listOfName = name.split(' ');
+
     if (listOfName.length > 3) {
       final String firstWord = listOfName.sublist(0, 3).join(' ');
       final String lastWord = listOfName.sublist(3).join(' ');
-
-      result.add(firstWord);
-      result.add(lastWord);
-      return result.join('\n');
+      return '$firstWord\n$lastWord';
     }
 
-    return '\n${viewModel.state.recipes[index].name}';
+    return '\n$name';
+  }
+
+  String getRecipeName(int index) {
+    return _formatRecipeName(viewModel.state.recipes[index].name);
   }
 
   String getSearchedRecipeName(int index) {
-    final List<String> listOfName = viewModel.state.searchedResult[index].name
-        .split(
-          ' ',
-        );
-    final List<String> result = [];
-    if (listOfName.length > 3) {
-      final String firstWord = listOfName.sublist(0, 3).join(' ');
-      final String lastWord = listOfName.sublist(3).join(' ');
-
-      result.add(firstWord);
-      result.add(lastWord);
-      return result.join('\n');
-    }
-
-    return '\n${viewModel.state.searchedResult[index].name}';
+    return _formatRecipeName(viewModel.state.searchedResult[index].name);
   }
 
   String getFilteredRecipeName(int index) {
-    final List<String> listOfName = viewModel.state.filteredRecipes[index].name
-        .split(
-          ' ',
-        );
-    final List<String> result = [];
-    if (listOfName.length > 3) {
-      final String firstWord = listOfName.sublist(0, 3).join(' ');
-      final String lastWord = listOfName.sublist(3).join(' ');
-
-      result.add(firstWord);
-      result.add(lastWord);
-      return result.join('\n');
-    }
-
-    return '\n${viewModel.state.filteredRecipes[index].name}';
+    return _formatRecipeName(viewModel.state.filteredRecipes[index].name);
   }
 }
