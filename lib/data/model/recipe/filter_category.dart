d39af3c937024d2e7b0049c783bf86ca54a95eca
Moon <1tjsen@naver.com>
2025-07-20 18:28:47 +0900

    refactor: FilterCategory toString 간소화

--- lib/data/model/recipe/filter_category.dart
+++ lib/data/model/recipe/filter_category.dart
@@ -13,18 +13,5 @@ enum FilterCategory {
   lunch;
 
   @override
-  String toString() {
-    return switch (this) {
-      FilterCategory.all => name.capitalizeFirstLetter(),
-      FilterCategory.cereal => name.capitalizeFirstLetter(),
-      FilterCategory.vegetables => name.capitalizeFirstLetter(),
-      FilterCategory.dinner => name.capitalizeFirstLetter(),
-      FilterCategory.chinese => name.capitalizeFirstLetter(),
-      FilterCategory.localDish => name.capitalizeFirstLetter(),
-      FilterCategory.fruit => name.capitalizeFirstLetter(),
-      FilterCategory.breakFast => name.capitalizeFirstLetter(),
-      FilterCategory.spanish => name.capitalizeFirstLetter(),
-      FilterCategory.lunch => name.capitalizeFirstLetter(),
-    };
-  }
+  String toString() => name.capitalizeFirstLetter();
 }
