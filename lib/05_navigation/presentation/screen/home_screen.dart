7c23697b63175603f66efb9ad4f8762b2f091ac7
Hagmin.Lee <iilhm2001@naver.com>
2025-07-22 03:32:41 +0900

    feat: HomeScreen 생성

---
+++ lib/05_navigation/presentation/screen/home_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_recipe_app/01_stateless_widget/ui/text_styles.dart';
+import 'package:flutter_recipe_app/04_ui_state_holder/presentation/component/filter_button.dart';
+import 'package:flutter_recipe_app/04_ui_state_holder/presentation/component/search_input_field.dart';
+
+import '../../../01_stateless_widget/ui/app_colors.dart';
+
+class HomeScreen extends StatelessWidget {
+  final String greeting = 'Hello Jega';
+  final String title = 'What are you cooking today?';
+
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.fromLTRB(30, 0, 30, 0),
+        child: Column(
+          children: [
+            SizedBox(height: 64),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(greeting, style: TextStyles.largeTextBold),
+                    SizedBox(height: 5),
+                    Text(
+                      title,
+                      style: TextStyles.smallerTextRegular.copyWith(
+                        color: AppColors.gray3,
+                      ),
+                    ),
+                  ],
+                ),
+                Spacer(),
+                Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    color: AppColors.secondary40,
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Icon(Icons.tag_faces_outlined),
+                ),
+              ],
+            ),
+            SizedBox(height: 30),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 40,
+                    margin: EdgeInsets.only(right: 20.0),
+                    child: SearchInputField(
+                      placeHolder: 'Search recipe',
+                      value: 'value',
+                      onValueChange: (value) {},
+                    ),
+                  ),
+                ),
+                FilterButton(onClick: () {}),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
