da30d7ff969f8560e7d76355913d97655d47c8d2
ParkGiTeak <rl980901@naver.com>
2025-07-22 02:48:16 +0900

    feat: Router StateFulShellRoute 적용

--- sesac7_flutter_recipe_app/lib/core/routing/router.dart
+++ sesac7_flutter_recipe_app/lib/core/routing/router.dart
@@ -1,13 +1,79 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_recipe_app/core/routing/routes.dart';
+import 'package:flutter_recipe_app/data/data_source/recipe_data_source/recipe_data_source.dart';
+import 'package:flutter_recipe_app/data/data_source/recipe_data_source/recipe_data_source_impl.dart';
+import 'package:flutter_recipe_app/data/repository/recipe_repository/recipe_repository.dart';
+import 'package:flutter_recipe_app/data/repository/recipe_repository/recipe_repository_impl.dart';
 import 'package:flutter_recipe_app/presentation/home/home_screen.dart';
+import 'package:flutter_recipe_app/presentation/main/main_screen.dart';
+import 'package:flutter_recipe_app/presentation/saved_recipe/saved_recipe_screen.dart';
+import 'package:flutter_recipe_app/presentation/saved_recipe/saved_recipe_view_model.dart';
 import 'package:flutter_recipe_app/presentation/sigin/sign_in_screen.dart';
 import 'package:flutter_recipe_app/presentation/sigin/sign_up_screen.dart';
 import 'package:flutter_recipe_app/presentation/splash/splash_screen.dart';
 import 'package:go_router/go_router.dart';
 
+final RecipeDataSource recipeDataSource = RecipeDataSourceImpl();
+final RecipeRepository recipeRepository = RecipeRepositoryImpl(
+  recipeDataSource,
+);
+
 final router = GoRouter(
   initialLocation: Routes.splash,
   routes: [
+    StatefulShellRoute.indexedStack(
+      builder: (context, state, navigationShell) {
+        return MainScreen(
+          body: navigationShell,
+          currentIndex: navigationShell.currentIndex,
+          onTap: (int index) {
+            navigationShell.goBranch(index);
+          },
+        );
+      },
+      branches: [
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: Routes.home,
+              builder: (context, state) {
+                return HomeScreen();
+              },
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: Routes.savedRecipes,
+              builder: (context, state) {
+                final SavedRecipeViewModel savedRecipeViewModel =
+                    SavedRecipeViewModel(
+                      recipeRepository,
+                    );
+                savedRecipeViewModel.fetchRecipes();
+                return ListenableBuilder(
+                  listenable: savedRecipeViewModel,
+                  builder: (context, build) {
+                    return FutureBuilder(future: savedRecipeViewModel.fetchRecipes(), builder: (context, snapshot) {
+                      // if (snapshot.connectionState == ConnectionState.waiting) {
+                      //   return const Center(
+                      //     child: CircularProgressIndicator(),
+                      //   );
+                      // }
+                      return SavedRecipeScreen(
+                        savedRecipeViewModel: savedRecipeViewModel,
+                      );
+                    });
+                  },
+                );
+              },
+            ),
+          ],
+        ),
+      ],
+    ),
     GoRoute(
       path: Routes.splash,
       builder: (context, state) {
@@ -24,12 +90,6 @@ final router = GoRouter(
       path: Routes.signUp,
       builder: (context, state) {
         return SignUpScreen();
-      },
-    ),
-    GoRoute(
-      path: Routes.home,
-      builder: (context, state) {
-        return HomeScreen();
       },
     ),
   ],
