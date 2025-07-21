d451dd76efddec0b60d6a4b61d484d9911491810
Moon <1tjsen@naver.com>
2025-07-21 18:00:12 +0900

    refactor: MaterialApp.router 사용하도록 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_recipe_app/presentation/screen/splash/splash_screen.dart';
+import 'package:flutter_recipe_app/core/routing/router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SplashScreen(),
+    return MaterialApp.router(
+      routerConfig: router,
     );
   }
 }
