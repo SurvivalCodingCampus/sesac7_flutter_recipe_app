ae12e482a4ea6125e40c02481fc7d987a7b97084
Moon <1tjsen@naver.com>
2025-07-21 19:51:58 +0900

    feat: HomeScreen 상단 구현

--- lib/presentation/screen/home/home_screen.dart
+++ lib/presentation/screen/home/home_screen.dart
@@ -1,10 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_recipe_app/presentation/component/button/search_filter_button.dart';
+import 'package:flutter_recipe_app/presentation/component/constants/component_constant.dart';
+import 'package:flutter_recipe_app/presentation/component/input/search_field.dart';
+import 'package:flutter_recipe_app/ui/app_colors.dart';
+import 'package:flutter_recipe_app/ui/text_styles.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsetsGeometry.fromLTRB(30, 20, 30, 15),
+          child: Column(
+            spacing: 30,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    spacing: 5,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Hello Jega',
+                        style: TextStyles.largeTextBold,
+                      ),
+                      Text(
+                        'What are you cooking today?',
+                        style: TextStyles.smallerTextRegular.copyWith(
+                          color: AppColors.gray3,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    width: 40,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: AppColors.secondary40,
+                      borderRadius: BorderRadius.circular(
+                        ComponentConstant.borderRadius,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                spacing: 20,
+                children: [
+                  Expanded(
+                    child: SearchField(
+                      placeholder: 'Search recipe',
+                      onValueChange: (value) {},
+                    ),
+                  ),
+                  SearchFilterButton(
+                    onTap: () {},
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
