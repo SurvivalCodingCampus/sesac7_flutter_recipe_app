eec442da33ff3380d5f605c4f6ffc7211147010a
Moon <1tjsen@naver.com>
2025-07-21 19:23:59 +0900

    feat: oauthButtonShadowAlpha 상수 추가

--- lib/presentation/component/constants/component_constant.dart
+++ lib/presentation/component/constants/component_constant.dart
@@ -4,4 +4,5 @@ abstract class ComponentConstant {
   static const double iconSize = 20;
   static const double cursorHeight = 17;
   static const double cursorWidth = 1;
+  static const int oauthButtonShadowAlpha = 255 ~/ 10;
 }
