60ebafd3fe453a8bfd44adc9ec82f821bd8e0405
Hagmin.Lee <iilhm2001@naver.com>
2025-07-22 15:14:12 +0900

    refactor: 불필요한 필드 제거 및 레이블 필드 추가

--- lib/04_ui_state_holder/presentation/screen/search_recipes_screen_state.dart
+++ lib/04_ui_state_holder/presentation/screen/search_recipes_screen_state.dart
@@ -3,22 +3,18 @@ import 'package:flutter_recipe_app/04_ui_state_holder/presentation/screen/filter
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'search_recipes_screen_state.freezed.dart';
-part 'search_recipes_screen_state.g.dart';
 
 @freezed
 abstract class SearchRecipesScreenState with _$SearchRecipesScreenState {
   const factory SearchRecipesScreenState({
     @Default([]) List<Recipe> recipes, // 전체 레시피 목록
-    @Default([]) List<Recipe> recipesSearchedByName, // 이름으로 한 번 분류된 레시피 목록
-    @Default([]) List<Recipe> recipesSearchedByChef, // 셰프 이름으로 한 번 분류된 레시피 목록
     @Default([]) List<Recipe> filteredRecipes,
     @Default([]) List<Recipe> searchedResult, // 최종 결과물
     @Default('') String query, // 검색어
     @Default(false) bool isLoading, // 로딩 중인지를 표시
+    @Default('Recent Search') String searchLabel,
+    @Default('') String countingLabel,
     @Default(FilterSearchBottomSheetState())
     FilterSearchBottomSheetState filterSearchState,
   }) = _SearchRecipesScreenState;
-
-  factory SearchRecipesScreenState.fromJson(Map<String, Object?> json) =>
-      _$SearchRecipesScreenStateFromJson(json);
 }
