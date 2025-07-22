1d3703f3112e9cbb318df0c1906f9d89c2a2dce3
ParkGiTeak <rl980901@naver.com>
2025-07-22 19:10:06 +0900

    fix: <compose> MediumButton, SmallButton 하위 Composable Modifier 수정

--- compose_recipe/app/src/main/java/com/example/recipeapp/presentation/component/button/SmallButton.kt
+++ compose_recipe/app/src/main/java/com/example/recipeapp/presentation/component/button/SmallButton.kt
@@ -2,7 +2,6 @@ package com.example.recipeapp.presentation.component.button
 
 import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
