f1a9173c5bb499ebcdf5f9b55143d89f5cbbcd83
ParkGiTeak <rl980901@naver.com>
2025-07-23 17:02:43 +0900

    feat: ScreenTitleBar 우측 끝에 메뉴버튼 추가 및 각 파라미터 null이 아닌경우에만 표출되도록 수정

--- sesac7_flutter_recipe_app/lib/presentation/component/title_bar/screen_title_bar.dart
+++ sesac7_flutter_recipe_app/lib/presentation/component/title_bar/screen_title_bar.dart
@@ -3,39 +3,53 @@ import 'package:flutter_recipe_app/ui/app_colors.dart';
 import 'package:flutter_recipe_app/ui/text_styles.dart';
 
 class ScreenTitleBar extends StatelessWidget {
-  final String title;
-  final VoidCallback backButtonClick;
+  final String? title;
+  final VoidCallback? backButtonClick;
+  final VoidCallback? menuButtonClick;
 
   const ScreenTitleBar({
     super.key,
-    required this.title,
-    required this.backButtonClick,
+    this.title,
+    this.backButtonClick,
+    this.menuButtonClick,
   });
 
   @override
   Widget build(BuildContext context) {
     return Row(
       crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        GestureDetector(
-          onTap: backButtonClick,
-          child: Icon(
-            Icons.arrow_back,
-            color: AppColors.black,
-            size: 20.0,
-          ),
-        ),
-        Spacer(),
+        backButtonClick == null
+            ? SizedBox(
+                width: 20.0,
+              )
+            : GestureDetector(
+                onTap: backButtonClick,
+                child: Icon(
+                  Icons.arrow_back,
+                  color: AppColors.black,
+                  size: 20.0,
+                ),
+              ),
         Text(
-          title,
+          title ?? '',
           style: TextStyles.searchScreenTitle,
           maxLines: 1,
           textAlign: TextAlign.center,
         ),
-        Spacer(),
-        SizedBox(
-          width: 20.0,
-        ),
+        menuButtonClick == null
+            ? SizedBox(
+                width: 20.0,
+              )
+            : GestureDetector(
+                onTap: menuButtonClick,
+                child: Icon(
+                  Icons.more_horiz,
+                  color: AppColors.black,
+                  size: 24.0,
+                ),
+              ),
       ],
     );
   }
