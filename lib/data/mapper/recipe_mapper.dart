e94a04339dd6ea24d9c6a7add62c50f6b6a8886b
Hagmin.Lee <iilhm2001@naver.com>
2025-07-24 02:27:18 +0900

    refactor: RecipeMapper에서 Ingredient amount 타입 변환 로직 추가

--- lib/data/mapper/recipe_mapper.dart
+++ lib/data/mapper/recipe_mapper.dart
@@ -7,7 +7,9 @@ extension DtoToModel on RecipeDto {
     final result = recipes?.where((e) => e.id == id).toList();
 
     final int convertedId;
+    final List<int> convertedAmount = [];
     final double convertedRating;
+    int index = 0;
 
     if (result?.first.id is int) {
       convertedId = result?.first.id as int;
@@ -21,16 +23,38 @@ extension DtoToModel on RecipeDto {
       convertedRating = -1.0;
     }
 
+    // Recipe에 포함된 ingredient가 없을 경우 해당 경로 실행
+    if (result?.first.ingredients == null) {
+      return Recipe(
+        category: result?.first.category ?? 'unknown',
+        id: convertedId,
+        name: result?.first.name ?? 'unknown',
+        image: result?.first.image ?? 'unknown',
+        chef: result?.first.chef ?? 'unknown',
+        cookingTime: result?.first.time ?? 'unknown',
+        rating: convertedRating,
+        ingredients: [],
+      );
+    }
+
+    for (var i = 0; i < result!.first.ingredients!.length; i++) {
+      if (result.first.ingredients?.first.amount is int) {
+        convertedAmount.add(result.first.ingredients?[i].amount as int);
+      } else {
+        convertedAmount.add(-1);
+      }
+    }
+
     return Recipe(
-      category: result?.first.category ?? 'unknown',
+      category: result.first.category ?? 'unknown',
       id: convertedId,
-      name: result?.first.name ?? 'unknown',
-      image: result?.first.image ?? 'unknown',
-      chef: result?.first.chef ?? 'unknown',
-      cookingTime: result?.first.time ?? 'unknown',
+      name: result.first.name ?? 'unknown',
+      image: result.first.image ?? 'unknown',
+      chef: result.first.chef ?? 'unknown',
+      cookingTime: result.first.time ?? 'unknown',
       rating: convertedRating,
       ingredients:
-          result?.first.ingredients
+          result.first.ingredients
               ?.map(
                 (dtoIngredient) => Ingredient(
                   id:
@@ -38,6 +62,7 @@ extension DtoToModel on RecipeDto {
                       -1, // num? -> int 변환
                   name: dtoIngredient.ingredient?.name ?? 'unknown',
                   image: dtoIngredient.ingredient?.image ?? 'unknown',
+                  amount: convertedAmount[index++],
                 ),
               )
               .toList() ??
