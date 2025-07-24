3cf6912831e07603e46797dbc925d0b1f8132623
Hagmin.Lee <iilhm2001@naver.com>
2025-07-25 00:09:56 +0900

    feat: MockRecipeDataSource, MockRecipeRepository 구현

---
+++ lib/data/repository/mock_recipe_repository_impl.dart
@@ -0,0 +1,63 @@
+import 'package:flutter_recipe_app/data/mapper/recipe_mapper.dart';
+import 'package:flutter_recipe_app/data/model/recipe.dart';
+import 'package:flutter_recipe_app/data/repository/recipe_repository.dart';
+
+import '../data_source/recipe_data_source.dart';
+
+class MockRecipeRepositoryImpl implements RecipeRepository {
+  final RecipeDataSource _dataSource;
+
+  MockRecipeRepositoryImpl({required RecipeDataSource dataSource})
+    : _dataSource = dataSource;
+
+  @override
+  Future<List<Recipe>> getRecipes() async {
+    final result = await _dataSource.getAllRecipes();
+
+    return result.map((e) => e.toModel(e.recipes?.first.id as int)).toList();
+  }
+
+  @override
+  Future<Recipe> getRecipeById(int id) async {
+    final listOfModels = await _dataSource.getAllRecipes();
+    final result = listOfModels
+        .map((e) => e.toModel(e.recipes?.first.id as int))
+        .toList();
+
+    return result.firstWhere((e) => e.id == id);
+  }
+
+  @override
+  Future<List<Recipe>> getRecipesByName(String name) async {
+    final listOfModels = await _dataSource.getAllRecipes();
+    final result = listOfModels
+        .map((e) => e.toModel(e.recipes?.first.id as int))
+        .toList();
+
+    return result
+        .where(
+          (e) => e.name
+              .replaceAll(' ', '')
+              .toLowerCase()
+              .contains(name.toLowerCase()),
+        )
+        .toList();
+  }
+
+  @override
+  Future<List<Recipe>> getRecipesByChef(String chef) async {
+    final listOfModels = await _dataSource.getAllRecipes();
+    final result = listOfModels
+        .map((e) => e.toModel(e.recipes?.first.id as int))
+        .toList();
+
+    return result
+        .where(
+          (e) => e.chef
+              .replaceAll(' ', '')
+              .toLowerCase()
+              .contains(chef.toLowerCase()),
+        )
+        .toList();
+  }
+}
