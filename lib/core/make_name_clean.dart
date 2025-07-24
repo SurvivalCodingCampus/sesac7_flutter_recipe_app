ea10808f79f25a30b8aec9d52ca71034e33d77be
Hagmin.Lee <iilhm2001@naver.com>
2025-07-25 00:38:50 +0900

    refactor: getRecipeName, _formatRecipeName 전역 함수로 변경

--- lib/core/make_name_clean.dart
+++ lib/core/make_name_clean.dart
@@ -1,39 +1,17 @@
-import 'package:flutter_recipe_app/presentation/screen/saved_recipes_screen.dart';
 
-import '../presentation/screen/search_recipes_screen.dart';
 
-extension SavedRecipesScreenNameOptimizer on SavedRecipesScreen {
-  String _formatRecipeName(String name) {
-    final List<String> listOfName = name.split(' ');
-
-    if (listOfName.length > 3) {
-      final String firstWord = listOfName.sublist(0, 3).join(' ');
-      final String lastWord = listOfName.sublist(3).join(' ');
-      return '$firstWord\n$lastWord';
-    }
-
-    return name;
-  }
-
-  String getRecipeName(String name) {
-    return _formatRecipeName(name);
-  }
+String getRecipeName(String name) {
+  return _formatRecipeName(name);
 }
 
-extension SearchRecipesScreenNameOptimizer on SearchRecipesScreen {
-  String _formatRecipeName(String name) {
-    final List<String> listOfName = name.split(' ');
+String _formatRecipeName(String name) {
+  final List<String> listOfName = name.split(' ');
 
-    if (listOfName.length > 3) {
-      final String firstWord = listOfName.sublist(0, 3).join(' ');
-      final String lastWord = listOfName.sublist(3).join(' ');
-      return '$firstWord\n$lastWord';
-    }
-
-    return name;
+  if (listOfName.length > 3) {
+    final String firstWord = listOfName.sublist(0, 3).join(' ');
+    final String lastWord = listOfName.sublist(3).join(' ');
+    return '$firstWord\n$lastWord';
   }
 
-  String getRecipeName(String name) {
-    return _formatRecipeName(name);
-  }
+  return name;
 }
