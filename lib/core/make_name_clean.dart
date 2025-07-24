d8baa024a873511dc4a5eccb81cb7388bb7546fc
Hagmin.Lee <iilhm2001@naver.com>
2025-07-25 00:11:43 +0900

    refactor: NameOptimizer를 extension으로 분리

--- lib/core/make_name_clean.dart
+++ lib/core/make_name_clean.dart
@@ -1,5 +1,9 @@
-class NameOptimizer {
-  static String _formatRecipeName(String name) {
+import 'package:flutter_recipe_app/presentation/screen/saved_recipes_screen.dart';
+
+import '../presentation/screen/search_recipes_screen.dart';
+
+extension SavedRecipesScreenNameOptimizer on SavedRecipesScreen {
+  String _formatRecipeName(String name) {
     final List<String> listOfName = name.split(' ');
 
     if (listOfName.length > 3) {
@@ -8,10 +12,28 @@ class NameOptimizer {
       return '$firstWord\n$lastWord';
     }
 
-    return '\n$name';
+    return name;
   }
 
-  static String getRecipeName(String name) {
+  String getRecipeName(String name) {
+    return _formatRecipeName(name);
+  }
+}
+
+extension SearchRecipesScreenNameOptimizer on SearchRecipesScreen {
+  String _formatRecipeName(String name) {
+    final List<String> listOfName = name.split(' ');
+
+    if (listOfName.length > 3) {
+      final String firstWord = listOfName.sublist(0, 3).join(' ');
+      final String lastWord = listOfName.sublist(3).join(' ');
+      return '$firstWord\n$lastWord';
+    }
+
+    return name;
+  }
+
+  String getRecipeName(String name) {
     return _formatRecipeName(name);
   }
 }
