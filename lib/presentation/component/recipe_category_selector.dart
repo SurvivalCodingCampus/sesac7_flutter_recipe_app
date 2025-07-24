b37b01683a4a70d929e74fecfa87119caeb7c8d9
Hagmin.Lee <iilhm2001@naver.com>
2025-07-25 00:10:22 +0900

    feat: RecipeCategorySelector 컴포넌트 생성

---
+++ lib/presentation/component/recipe_category_selector.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+import '../../core/category.dart';
+import '../../ui/app_colors.dart';
+import '../../ui/text_styles.dart';
+
+class RecipeCategorySelector extends StatelessWidget {
+  final String category;
+  final void Function(String category) onSelectCategory;
+
+  const RecipeCategorySelector({
+    super.key,
+    required this.category,
+    required this.onSelectCategory,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 51,
+      padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ...List.generate(Category.values.length, (index) {
+              return GestureDetector(
+                onTap: () {
+                  onSelectCategory(Category.values[index].name);
+                },
+                child: AnimatedContainer(
+                  // width: 54,
+                  height: 31,
+                  padding: EdgeInsets.fromLTRB(20, 7, 20, 7),
+                  margin: EdgeInsets.symmetric(horizontal: 9),
+                  decoration: BoxDecoration(
+                    color: category == Category.values[index].name
+                        ? AppColors.primary100
+                        : AppColors.white,
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  duration: Duration(milliseconds: 150),
+                  child: Text(
+                    Category.values[index].name,
+                    style: TextStyles.smallerTextBold.copyWith(
+                      color: category == Category.values[index].name
+                          ? AppColors.white
+                          : AppColors.primary80,
+                    ),
+                  ),
+                ),
+              );
+            }),
+          ],
+        ),
+      ),
+    );
+  }
+}
