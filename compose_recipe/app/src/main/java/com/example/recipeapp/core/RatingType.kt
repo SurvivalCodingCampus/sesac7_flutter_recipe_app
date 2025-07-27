33effbcbedb834dcce66e33340ca24621d3ff847
ParkGiTeak <rl980901@naver.com>
2025-07-27 14:43:01 +0900

    feat: <compose> RatingType Enum 정의

---
+++ compose_recipe/app/src/main/java/com/example/recipeapp/core/RatingType.kt
@@ -0,0 +1,12 @@
+package com.example.recipeapp.core
+
+enum class RatingType(
+    val value: Int
+) {
+    GRADE_ZERO(0),
+    GRADE_ONE(1),
+    GRADE_TWO(2),
+    GRADE_THREE(3),
+    GRADE_FOUR(4),
+    GRADE_FIVE(5)
+}
