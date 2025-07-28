265aec79e622a8c4f2e95621c5f03f28ac9c188b
Hagmin.Lee <iilhm2001@naver.com>
2025-07-28 20:28:14 +0900

    refactor: MockSplashScreenState를 SplashScreenState로 이름 변경

--- lib/features/splash/presentation/screen/splash_screen_view_model.dart
+++ lib/features/splash/presentation/screen/splash_screen_view_model.dart
@@ -2,16 +2,16 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_recipe_app/features/splash/data/domain/use_case/fetch_system_settings_use_case.dart';
-import 'package:flutter_recipe_app/features/splash/presentation/screen/mock_splash_screen_state.dart';
 import 'package:flutter_recipe_app/features/splash/presentation/screen/splash_screen_action.dart';
 import 'package:flutter_recipe_app/features/splash/presentation/screen/splash_screen_event.dart';
+import 'package:flutter_recipe_app/features/splash/presentation/screen/splash_screen_state.dart';
 
 class SplashScreenViewModel with ChangeNotifier {
   final FetchSystemSettingsUseCase _fetchSystemSettingsUseCase;
 
-  MockSplashScreenState _state = const MockSplashScreenState();
+  SplashScreenState _state = const SplashScreenState();
 
-  MockSplashScreenState get state => _state;
+  SplashScreenState get state => _state;
 
   final _eventController = StreamController<SplashScreenEvent>();
 
