a602ed6626a8507aaad23779ab9f430447378ea5
jason lim <7newer@gmail.com>
2025-07-29 23:21:47 +0900

    feat: presentation viewModel->screen 과정 확인

--- lib/presentation/screen/saved_screen/saved_recipes_screen.dart
+++ lib/presentation/screen/saved_screen/saved_recipes_screen.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_recipe_app/presentation/screen/saved_screen/saved_recipes_view_model.dart';
+
+import '../../../data/data_source/recipe_data_source_impl.dart';
+import '../../../data/repository/recipe_repository_impl.dart';
 
 class SavedRecipesScreen extends StatelessWidget {
-  const SavedRecipesScreen({super.key});
+  final SavedRecipesViewModel savedRecipedViewModel;
+
+  // 생성자로 viewModel을 주입 받음
+  const SavedRecipesScreen({super.key, required this.savedRecipedViewModel});
 
   @override
   Widget build(BuildContext context) {
@@ -9,11 +16,37 @@ class SavedRecipesScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text(''),
       ),
-      body: Container(),
+      body: ListenableBuilder(
+        listenable: savedRecipedViewModel,
+        builder: (context, child) {
+          return Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: SingleChildScrollView(
+                child: Text(
+                    'Recipes: ${savedRecipedViewModel.rawResultString}'
+                  ),
+                ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
 
 void main() async {
-  
+  final savedRecipesViewModel = SavedRecipesViewModel(
+    recipeRepository: RecipeRepositoryImpl(
+      RecipeDataSourceImpl(),
+    ),
+  );
+
+  await (savedRecipesViewModel.fetchSavedRecipes() as Future<void>);
+
+  runApp(
+    MaterialApp(
+      home: SavedRecipesScreen(savedRecipedViewModel: savedRecipesViewModel),
+    ),
+  );
 }
--- lib/presentation/screen/saved_screen/saved_recipes_view_model.dart
+++ lib/presentation/screen/saved_screen/saved_recipes_view_model.dart
@@ -3,15 +3,27 @@ import 'package:flutter_recipe_app/data/model/recipes.dart';
 
 import '../../../data/repository/recipe_repository.dart';
 
-class SavedRecipesScreen with ChangeNotifier {
+class SavedRecipesViewModel with ChangeNotifier {
   final RecipeRepository _recipeRepository;
 
-  SavedRecipesScreen({required RecipeRepository recipeRepository})
+  SavedRecipesViewModel({required RecipeRepository recipeRepository})
     : _recipeRepository = recipeRepository;
 
   // 저장된 레시피 리스트
   List<Recipes> _savedRecipes = [];
 
   // 저장된 레시피 리스트에 레시피 추가
-  List<Recipes> get savedRecipes => _savedRecipes;
+  List<Recipes> get savedRecipes => List.unmodifiable(_savedRecipes);
+
+  String? _rawResultString;
+
+  String? get rawResultString => _rawResultString;
+
+  void fetchSavedRecipes() async {
+
+    final result = await _recipeRepository.getRecipes();
+    print(result);
+    _rawResultString = result.toString();
+    notifyListeners();
+  }
 }
