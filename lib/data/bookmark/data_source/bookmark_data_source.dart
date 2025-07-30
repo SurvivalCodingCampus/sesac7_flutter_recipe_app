6641a459870087c8bf3858f6609f122368cbd077
Hagmin.Lee <iilhm2001@naver.com>
2025-07-30 20:54:23 +0900

    refactor: 북마크 관련 DataSource 및 Repository 제거

--- lib/data/bookmark/data_source/bookmark_data_source.dart
+++
@@ -1,5 +0,0 @@
-import '../../../core/data/recipe/dto/recipe_dto.dart';
-
-abstract interface class BookmarkDataSource {
-  Future<List<RecipeDto>> getSavedRecipes();
-}
--- lib/data/bookmark/domain/repository/bookmark_repository.dart
+++
@@ -1,5 +0,0 @@
-import '../../../../core/data/recipe/domain/model/recipe.dart';
-
-abstract interface class BookmarkRepository {
-  Future<List<Recipe>> getSavedRecipes();
-}
