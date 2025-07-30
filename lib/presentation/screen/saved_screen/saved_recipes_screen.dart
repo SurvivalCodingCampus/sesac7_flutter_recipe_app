8a123561a0a0a6b483b21a2cf5790eb1b3e14ecc
jason lim <7newer@gmail.com>
2025-07-30 18:15:53 +0900

    fix: 수정

--- lib/presentation/screen/saved_screen/saved_recipes_screen.dart
+++ lib/presentation/screen/saved_screen/saved_recipes_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_recipe_app/presentation/screen/saved_screen/saved_recipe
 import '../../../data/data_source/recipe_data_source_impl.dart';
 import '../../../data/repository/recipe_repository_impl.dart';
 import '../../component/card/recipe_card.dart';
+import '../../component/inputfield/input_field.dart';
 
 class SavedRecipesScreen extends StatelessWidget {
   final SavedRecipesViewModel savedRecipedViewModel;
@@ -17,39 +18,58 @@ class SavedRecipesScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Saved Recipes'),
       ),
-      body: ListenableBuilder(
-        listenable: savedRecipedViewModel,
-        builder: (context, child) {
-          if (savedRecipedViewModel.isLoading) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          if (savedRecipedViewModel.errorMessage != null) {
-            return Center(
-              child: Text(
-                savedRecipedViewModel.errorMessage!,
-                style: const TextStyle(color: Colors.red, fontSize: 16),
-                textAlign: TextAlign.center,
-              ),
-            );
-          }
-          return ListView.builder(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            // 상하좌우 패딩 또는 마진 // 동적으로 이미지 변경
-            itemCount: savedRecipedViewModel.savedRecipes.length,
-            itemBuilder: (context, index) {
-              final recipe = savedRecipedViewModel.savedRecipes[index];
-              return Padding(
-                padding: EdgeInsets.symmetric(vertical: 10),
-                child: RecipeCard(
-                  recipe: recipe,
-                  onBookmarkPressed: () {},
-                ),
-              );
-            },
-          );
-        },
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+           Padding(
+             padding: const EdgeInsets.all(8.0),
+             child: InputField(
+              label: 'Label',
+              placeHolder: 'Placeholder',
+              value: 'Value',
+              onValueChange: (String value) {
+                print(value);
+              },
+                       ),
+           ),
+          SizedBox(height: 16),
+          Expanded(
+            child: ListenableBuilder(
+              listenable: savedRecipedViewModel,
+              builder: (context, child) {
+                if (savedRecipedViewModel.isLoading) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                if (savedRecipedViewModel.errorMessage != null) {
+                  return Center(
+                    child: Text(
+                      savedRecipedViewModel.errorMessage!,
+                      style: const TextStyle(color: Colors.red, fontSize: 16),
+                      textAlign: TextAlign.center,
+                    ),
+                  );
+                }
+                return ListView.builder(
+                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  // 상하좌우 패딩 또는 마진 // 동적으로 이미지 변경
+                  itemCount: savedRecipedViewModel.savedRecipes.length,
+                  itemBuilder: (context, index) {
+                    final recipe = savedRecipedViewModel.savedRecipes[index];
+                    return Padding(
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      child: RecipeCard(
+                        recipe: recipe,
+                        onBookmarkPressed: () {},
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
