86d65317e208e2263fb20c5140fe211e0ee9b71d
Hagmin.Lee <iilhm2001@naver.com>
2025-07-31 09:46:21 +0900

    refactor: MockBookmarkRepository에서 getAllRecipes 제거 및 getSavedRecipes 반환 타입 nullable로 변경

--- lib/data/bookmark/domain/repository/mock_bookmark_repository.dart
+++ lib/data/bookmark/domain/repository/mock_bookmark_repository.dart
@@ -1,9 +1,9 @@
 import '../../../../core/data/recipe/domain/model/recipe.dart';
 
 abstract interface class MockBookmarkRepository {
-  List<Recipe> getAllRecipes();
+  // List<Recipe> getAllRecipes();
 
-  List<Recipe> getSavedRecipes();
+  List<Recipe>? getSavedRecipes();
 
   // void addRecipe(Recipe recipe);
 
